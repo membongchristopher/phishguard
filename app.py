782f9d954d0eb7a5db26a2b50842cd6aeee41228
Christopher Membong <christophermembong@gmail.com>
2026-03-23 02:27:30 +0000

    fix port for deployment

--- app.py
+++ app.py
@@ -325,4 +325,5 @@ def analyze():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
